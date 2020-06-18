0721f31500bd8d9aa1223a564971d4f522c3421d
oxygensu <835241707@qq.com>
2020-06-18 16:42:14 +0800

    update two navigation background color

--- Post-It/app.py
+++ Post-It/app.py
@@ -24,8 +24,8 @@ class MyWindow(QMainWindow):
         self.main_widget.setLayout(self.main_layout)  # 将主部件设置为网格布局
 
         # 初始化导航栏和导航栏界面
-        self.init_navigaton_bar()
-        self.init_navigaton_interface()
+        self.init_navigation_bar()
+        self.init_navigation_interface()
         self.clicked_1()  # 默认显示界面1
 
         # 将初始化完成的左侧、右侧空间加入整体空间的网格布局
@@ -34,7 +34,7 @@ class MyWindow(QMainWindow):
         self.main_layout.addWidget(self.right_widget2, 0, 1, 1, 6)
         self.setCentralWidget(self.main_widget)  # 设置窗口主部件
 
-    def init_navigaton_bar(self):
+    def init_navigation_bar(self):
         self.left_widget = QWidget()  # 创建左侧部件
         self.left_widget.setObjectName("navigaton_bar")  # 左侧部件对象命名
         self.left_layout = QGridLayout()  # 创建网格布局对象
@@ -46,9 +46,9 @@ class MyWindow(QMainWindow):
         self.left_layout.addWidget(self.button1)
         self.left_layout.addWidget(self.button2)
 
-    def init_navigaton_interface(self):
+    def init_navigation_interface(self):
         self.right_widget1 = QWidget()  # 创建右侧界面1
-        self.right_widget1.setStyleSheet("QWidget { background-color: Red}")
+        self.right_widget1.setStyleSheet("QWidget { background-color: Green}")
         self.right_layout1 = QGridLayout()  # 创建网格布局对象1
         self.right_widget1.setLayout(self.right_layout1)  # 设置右侧界面1的布局为网格布局
         self.label1 = QLabel()
@@ -56,7 +56,7 @@ class MyWindow(QMainWindow):
         self.right_layout1.addWidget(self.label1)
 
         self.right_widget2 = QWidget()  # 创建右侧界面2
-        self.right_widget2.setStyleSheet("QWidget { background-color: Blue}")
+        self.right_widget2.setStyleSheet("QWidget { background-color: Orange}")
         self.right_layout2 = QGridLayout()  # 创建网格布局对象2
         self.right_widget2.setLayout(self.right_layout2)  # 设置右侧界面2的布局为网格布局
         self.label2 = QLabel()
